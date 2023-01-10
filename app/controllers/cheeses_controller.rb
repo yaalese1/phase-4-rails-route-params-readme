2b1641a8043686f176bb11b2e9cd122a783a4cc3
Yetunde Alese <yaalese1@gmail.com>
2023-01-10 13:03:51 -0500

    commit tree ac6ef984b1f71eef0f63c30f2f1337c00b94e236

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,10 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  def show 
+   
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+  end
 
 end
